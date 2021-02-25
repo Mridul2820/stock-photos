f23910c05b2a21c28714aa03e0b1a8a5f1cb4c95
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-25 11:34:19 +0530

    base setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './components/Photo'
 import './App.scss';
+
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
 
 const App = () => {
     return (
